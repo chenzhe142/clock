2a57fdad78624afc9c2551f0ae405ebe8feee17f
chenzhe142 <chenzhe142@163.com>
2015-01-20 09:55:56 -0600

    Jan.19

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	private const float
+		hourToDegree = 360f / 12f,
+		minuteToDegree = 360f / 60f,
+		secondToDegree = 360f / 60f;
+
+	public Transform hour, minute, second;
+
+	public bool analog;
+
+	// Update is called once per frame
+	void Update () {
+		if (analog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			hour.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hourToDegree);
+			minute.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minuteToDegree);
+			second.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -secondToDegree);
+				
+		} else {
+			DateTime time = DateTime.Now;
+			hour.localRotation = Quaternion.Euler (0f, 0f, time.Hour * -hourToDegree);
+			minute.localRotation = Quaternion.Euler (0f, 0f, time.Minute * -minuteToDegree);
+			second.localRotation = Quaternion.Euler (0f, 0f, time.Second * -secondToDegree);
+		}
+	}
+}
